936414d811228c3f8f85adb7e11ba2cfb63377f2
Tobias Dedorsson <50805430+tobionesies@users.noreply.github.com>
2023-05-11 14:49:35 +0200

    Qick Fix app.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -2,13 +2,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-<<<<<<< HEAD
   render(<App />);
   const header = screen.getByText(/Sten-Sax-Påse/i);
   expect(header).toBeInTheDocument();
-=======
-  // render(<App />);
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
->>>>>>> 4e44d60c67b21dc9e46bf35ecdf8b2900bb80b66
 });
