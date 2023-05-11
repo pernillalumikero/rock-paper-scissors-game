b91e9a02aa6dc2c41b25dbddd99e51a2f61b8b6a
Pernilla Lumikero <113108559+pernillalumikero@users.noreply.github.com>
2023-05-11 22:20:54 +0200

    added more test to score

--- src/components/score/__test__/Score.test.js
+++ src/components/score/__test__/Score.test.js
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react"
 import Score from "../Score"
 
 describe("Score", () => {
-    test("Should add score on win", () => {
+    test("Should add score to player1 on win", () => {
         //Given
         const player1 = {
             name: "Spelare1",
@@ -23,6 +23,55 @@ describe("Score", () => {
         const score = screen.getByText(/Ställningen är/i)
 
         //Then
-        expect(score).toHaveTextContent(/Ställningen är Spelare1: 1/i)
+        expect(score).toHaveTextContent(/Spelare1: 1/i)
+    })
+
+    test("Should add score to player2 on win", () => {
+        //Given
+        const player1 = {
+            name: "Spelare1",
+            choice: "Sax",
+            isWinner: false
+        }
+
+        const player2 = {
+            name: "Spelare2",
+            choice: "Sten",
+            isWinner: true
+        }
+
+        const result = "Spelare2 vann!"
+
+        //When
+        render(<Score result={result} player1={player1} player2={player2}/>)
+        const score = screen.getByText(/Ställningen är/i)
+
+        //Then
+        expect(score).toHaveTextContent(/Spelare2: 1/i)
+    })
+
+    test("Add no scores on tie", () => {
+        //Given
+        const player1 = {
+            name: "Spelare1",
+            choice: "Sax",
+            isWinner: false
+        }
+
+        const player2 = {
+            name: "Spelare2",
+            choice: "Sax",
+            isWinner: false
+        }
+
+        const result = "Oavgjort!"
+
+        //When
+        render(<Score result={result} player1={player1} player2={player2}/>)
+        const score = screen.getByText(/Ställningen är/i)
+
+        //Then
+        expect(score).toHaveTextContent(/Spelare1: 0 Spelare2: 0/i)
     })
 })
+
